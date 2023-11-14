65ad29225933fd7e8bed26df0e8cb9bfb9c39d31
GiovaniBenvenutti <giovanibenvenutti2010@hotmail.com>
2023-11-14 13:07:26 -0300

    tentando implantar odatapicker

--- src/app/shared/data-picker/data-picker.component.ts
+++ src/app/shared/data-picker/data-picker.component.ts
@@ -1,27 +1,64 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Input, Output, forwardRef} from '@angular/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatNativeDateModule} from '@angular/material/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
-/** @title Datepicker with min & max validation */
 @Component({
   selector: 'app-data-picker',
   templateUrl: './data-picker.component.html',
   styleUrls: ['./data-picker.component.css'],
-
   standalone: true,
   imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DataPickerComponent),
+      multi: true
+    }
+  ]
 })
-export class DataPickerComponent {
+export class DataPickerComponent implements ControlValueAccessor {
+  @Output() dataLevantamento = new EventEmitter<Date>(); 
+  @Input() data: Date = new Date();
+  
   minDate: Date;
   maxDate: Date;
+  ValueFieldComponent!: Date;
+  static ValueFieldComponent: Date;
 
   constructor() {
-    // Set the minimum to January 1st 20 years in the past and December 31st a year in the future.
     const currentYear = new Date().getFullYear();
-    this.minDate = new Date(currentYear - 20, 0, 1);
-    this.maxDate = new Date(currentYear + 1, 11, 31);
+    this.minDate = new Date(currentYear - 50, 0, 1);
+    this.maxDate = new Date(currentYear + 10, 11, 31);
+  }
+
+  ngOnInit(){
+    this.data = DataPickerComponent.ValueFieldComponent;
+    if(this.data){
+      this.emitData();
+    }
+  }
+
+  emitData(): void {
+    this.dataLevantamento.emit(this.data);
+  }
+
+  // Métodos da interface ControlValueAccessor
+  writeValue(obj: any): void {
+    this.data = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.dataLevantamento.subscribe(fn);
+  }
+
+  registerOnTouched(fn: any): void {
+    // Você pode implementar este método se precisar
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    // Você pode implementar este método se precisar
   }
 }
-
