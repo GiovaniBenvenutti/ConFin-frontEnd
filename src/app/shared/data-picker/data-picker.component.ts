a0f7b4a7870bfb4a2394338725474cbdad95e2ea
GiovaniBenvenutti <giovanibenvenutti2010@hotmail.com>
2023-12-05 12:53:15 -0300

    acho que ta resolvido o datapicker

--- src/app/shared/data-picker/data-picker.component.ts
+++ src/app/shared/data-picker/data-picker.component.ts
@@ -1,8 +1,6 @@
 import {Component, EventEmitter, Inject, Input, OnInit, Output, forwardRef} from '@angular/core';
 import {MatNativeDateModule} from '@angular/material/core';
-
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
-import { DatePipe } from '@angular/common';
 
 import {
   MAT_MOMENT_DATE_FORMATS,
@@ -28,12 +26,7 @@ import { ControlValueAccessor, FormsModule } from '@angular/forms';
   imports: [MatFormFieldModule, MatInputModule, MatNativeDateModule, MatDatepickerModule, FormsModule,
             MatFormFieldModule, MatInputModule, MatDatepickerModule, MatButtonModule],
   providers: [
-    // The locale would typically be provided on the root module of your application. We do it at
-    // the component level here, due to limitations of our example generation script.
     {provide: MAT_DATE_LOCALE, useValue: 'ja-JP'},
-    // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
-    // `MatMomentDateModule` in your applications root module. We provide it at the component level
-    // here, due to limitations of our example generation script.
     {
       provide: DateAdapter,
       useClass: MomentDateAdapter,
@@ -48,8 +41,7 @@ export class DataPickerComponent implements ControlValueAccessor, OnInit {
   @Input() dataDoLevantameto: Date = new Date();  
   @Output() dataSelecionada = new EventEmitter<Date>();
 
-  constructor(private datePipe: DatePipe, 
-      private _adapter: DateAdapter<any>,
+  constructor(private _adapter: DateAdapter<any>,
       private _intl: MatDatepickerIntl,
       @Inject(MAT_DATE_LOCALE) private _locale: string) { }
 
@@ -59,64 +51,41 @@ export class DataPickerComponent implements ControlValueAccessor, OnInit {
 
   // Função para armazenar o toque no controle
   private onTouched = () => {};
-  //datePipe: any;
-
+  
   writeValue(value: any): void {
     this.dataDoLevantameto = value;
   }
-
   registerOnChange(fn: any): void {
     this.onChange = fn;
   }
-
   registerOnTouched(fn: any): void {
     this.onTouched = fn;
   }
 
-  // Método chamado quando a data é alterada
   onDateChange(event: MatDatepickerInputEvent<Date>) {
     if (event.value != undefined) {   
       this.dataDoLevantameto = event.value;
-      this.datePipe.transform(this.dataDoLevantameto, 'yyyy-MM-dd') || '';
       this.onChange(event.value);
       this.onTouched();
       this.dataSelecionada.emit(this.dataDoLevantameto);
     }
   }
   
-
-  ngOnInit() {
-    this.updateCloseButtonLabel('カレンダーを閉じる');
-    this.formataData();
-  }
-
-  french() {
-    this._locale = 'fr';
-    this._adapter.setLocale(this._locale);
-    this.updateCloseButtonLabel('Fermer le calendrier');
-  }
-
   formataData() {
-    this._locale = 'fr';
+    this._locale = 'br';
     this._adapter.setLocale(this._locale);
-    this.updateCloseButtonLabel('Fermer le calendrier');
-  }
-
-  updateCloseButtonLabel(label: string) {
-    this._intl.closeCalendarLabel = label;
-    this._intl.changes.next();
   }
 
   getDateFormatString(): string {
-    if (this._locale === 'ja-JP') {
-      return 'YYYY/MM/DD';
-    } else if (this._locale === 'fr') {
+    if (this._locale === 'br') {
       return 'DD/MM/YYYY';
     }
     return '';
-  }
-
+  }  
   
+  ngOnInit() {
+    this.formataData();
+  }
 }
 
 
