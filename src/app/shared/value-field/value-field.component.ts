0338a49ee1aa2fadfaf03199193720aba66b3ab6
GiovaniBenvenutti <giovanibenvenutti2010@hotmail.com>
2023-12-05 18:42:57 -0300

    :)

--- src/app/shared/value-field/value-field.component.ts
+++ src/app/shared/value-field/value-field.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
@@ -14,4 +14,11 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 })
 export class ValueFieldComponent {
   hide = false; //true;
+
+  @Input() valorlevantado: number = 0;
+
+
+
+
+  
 }
